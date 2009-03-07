11c803d86955062d75a9b210160b2e314979765d
nolssi <nolssi@1e2eb27e-0179-11de-9992-cbdf800730d7>
2009-03-07 11:46:38 +0000

    Enum for GCGC columns filled.

--- src/guineu/data/datamodels/CGCGColumnName.java
+++ src/guineu/data/datamodels/CGCGColumnName.java
@@ -18,25 +18,32 @@
 
 package guineu.data.datamodels;
 
-/**
- *
- * @author bicha
- */
+
 public enum CGCGColumnName {
-//GCGC-Tof files
-            /*columns_mol.add("Selection");
-            columns_mol.add("ID");
-            columns_mol.add("RT1");
-            columns_mol.add("RT2");
-            columns_mol.add("RTI");
-            columns_mol.add("N Found");
-            columns_mol.add("Max similarity");
-            columns_mol.add("Mean similarity");
-            columns_mol.add("Similarity std dev");
-            columns_mol.add("Metabolite name");
-            columns_mol.add("Metabolite all names");
-            columns_mol.add("Pubchem ID");
-            columns_mol.add("Mass");
-            columns_mol.add("Difference");
-            columns_mol.add("Spectrum");*/
+    SELECTION ("Selection"),
+    ID ("ID"),
+    RT1 ("RT1"),
+    RT2 ("RT2"),
+    RTI ("RTI"),
+    NFOUND ("N Found"),
+    MAXSIM ("Max similarity"),
+    MEANSIM ("Mean similarity"),
+    SIMSTD ("Similarity std dev"),
+    NAME ("Metabolite name"),
+    ALLNAMES ("Metabolite all names"),
+    PUBCHEM ("Pubchem ID"),
+    MASS ("Mass"),
+    DIFFERENCE ("Difference"),
+    SPECTRUM ("Spectrum");
+
+    private final String columnName;
+
+    CGCGColumnName(String columnName){
+        this.columnName = columnName;
+    }
+
+    public String getColumnName(){
+        return this.columnName;
+    }
 }
+
