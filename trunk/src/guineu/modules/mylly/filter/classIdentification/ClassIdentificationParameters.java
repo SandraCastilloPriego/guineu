1d207d88dd2361f2e0cf084a466cee6838a96a3b
nolssi <nolssi@1e2eb27e-0179-11de-9992-cbdf800730d7>
2009-09-04 12:51:10 +0000

    Mylly: class identification recursive function to read the CLIC rules

---
+++ trunk/src/guineu/modules/mylly/filter/classIdentification/ClassIdentificationParameters.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2007-2008 VTT Biotechnology
+ * This file is part of Guineu.
+ *
+ * Guineu is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Guineu is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Guineu; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package guineu.modules.mylly.filter.classIdentification;
+
+import guineu.data.Parameter;
+import guineu.data.ParameterType;
+import guineu.data.impl.SimpleParameter;
+import guineu.data.impl.SimpleParameterSet;
+
+/**
+ *
+ * @author scsandra
+ */
+public class ClassIdentificationParameters extends SimpleParameterSet {
+
+
+	public static final Parameter suffix = new SimpleParameter(
+            ParameterType.STRING, "Suffix: ",
+            "Suffix", null, "-Class Identification Filter", null);
+
+
+    public static final Parameter fileNames = new SimpleParameter(
+            ParameterType.FILE_NAME, "Filter File: ",
+            "Class Identification File", null, "Class Identification File", null);
+	
+	
+	public ClassIdentificationParameters() {
+		super(new Parameter[]{suffix, fileNames});
+	}
+}
