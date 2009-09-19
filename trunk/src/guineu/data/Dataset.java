0979692c8c1601825c336b86bd402ccefc7377d3
nolssi <nolssi@1e2eb27e-0179-11de-9992-cbdf800730d7>
2009-09-19 18:04:55 +0000

    --force-log

--- trunk/src/guineu/data/Dataset.java
+++ trunk/src/guineu/data/Dataset.java
@@ -13,7 +13,7 @@
  *
  * You should have received a copy of the GNU General Public License along with
  * Guineu; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
- * Fifth Floor, Boston, MA 02110-1301 USA 
+ * Fifth Floor, Boston, MA 02110-1301 USA
  */
 package guineu.data;
 
