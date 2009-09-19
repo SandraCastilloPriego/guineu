e6877fa9382d0456ece69f08fbda43cb742c7193
nolssi <nolssi@1e2eb27e-0179-11de-9992-cbdf800730d7>
2009-09-19 18:29:12 +0000

    --force-log

--- src/guineu/modules/file/openLCMSDatasetFile/OpenFileTask.java
+++ src/guineu/modules/file/openLCMSDatasetFile/OpenFileTask.java
@@ -15,12 +15,13 @@
  * Guineu; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
  * Fifth Floor, Boston, MA 02110-1301 USA
  */
+package guineu.modules.file.openLCMSDatasetFile;
 
-package guineu.modules.file.openExperimentsData;
-
-
-import guineu.data.datamodels.ExperimentDataModel;
-import guineu.data.parser.impl.ExperimentParserXLS;
+import guineu.data.datamodels.DatasetLCMSDataModel;
+import guineu.data.parser.impl.LCMSParserCSV;
+import guineu.data.parser.impl.LCMSParserXLS;
+import guineu.data.impl.SimpleLCMSDataset;
+import guineu.data.parser.Parser;
 import guineu.desktop.Desktop;
 import guineu.taskcontrol.Task;
 import guineu.util.Tables.DataTable;
@@ -28,70 +29,113 @@ import guineu.util.Tables.DataTableModel;
 import guineu.util.Tables.impl.PushableTable;
 import guineu.util.internalframe.DataInternalFrame;
 import java.awt.Dimension;
+import java.io.IOException;
 
 /**
  *
  * @author scsandra
  */
 public class OpenFileTask implements Task {
-    
-    private String fileDir;    
-    private TaskStatus status = TaskStatus.WAITING;
-    private String errorMessage;      
-    private Desktop desktop;  
-    private ExperimentParserXLS parser;
-    
-    public OpenFileTask(String fileDir, Desktop desktop){
-        if(fileDir != null)
-            this.fileDir = fileDir;
-        this.desktop = desktop;
-        parser = new ExperimentParserXLS(fileDir); 
-    }
-    public String getTaskDescription() {
-        return "Opening Experiment File... ";
-    }
 
-    public double getFinishedPercentage() {
-        return parser.getProgress();
-    }
+	private String fileDir;
+	private TaskStatus status = TaskStatus.WAITING;
+	private String errorMessage;
+	private Desktop desktop;
+	private Parser parser;
 
-    public TaskStatus getStatus() {
-        return status;
-    }
+	public OpenFileTask(String fileDir, Desktop desktop) {
+		if (fileDir != null) {
+			this.fileDir = fileDir;
+		}
+		this.desktop = desktop;
+	}
 
-    public String getErrorMessage() {
-        return errorMessage;
-    }
+	public String getTaskDescription() {
+		return "Opening File... ";
+	}
 
-    public void cancel() {
-        status = TaskStatus.CANCELED;
-    }
+	public double getFinishedPercentage() {
+		if (parser != null) {
+			return parser.getProgress();
+		} else {
+			return 0.0f;
+		}
+	}
 
-    public void run() {
-        try{          
-            this.openFile();          
-        }catch(Exception e) {
-            status = TaskStatus.ERROR;
-            errorMessage = e.toString();
-            return;
-        }
-    }
-    
-    public void openFile(){
-        status = TaskStatus.PROCESSING;        
-        try {
-            parser.fillData();
-            desktop.AddNewFile(parser.getData());
-           
-            //creates internal frame with the table
-            DataTableModel model = new ExperimentDataModel(parser.getData());                       
-            DataTable table = new PushableTable(model);            
-            DataInternalFrame frame = new DataInternalFrame(parser.getDatasetName(), table.getTable(), new Dimension(800,800));
+	public TaskStatus getStatus() {
+		return status;
+	}
 
-            desktop.addInternalFrame(frame);
-            frame.setVisible(true);      
-        } catch (Exception ex) {                
-        }               
-        status = TaskStatus.FINISHED;
-    }
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void cancel() {
+		status = TaskStatus.CANCELED;
+	}
+
+	public void run() {
+		try {
+			status = TaskStatus.PROCESSING;
+			this.openFile();
+			status = TaskStatus.FINISHED;
+		} catch (Exception e) {
+			status = TaskStatus.ERROR;
+			errorMessage = e.toString();
+			return;
+		}
+	}
+
+	public void openFile() {
+		if (fileDir.matches(".*xls")) {
+			try {
+				Parser parserName = new LCMSParserXLS(fileDir, null);
+				String[] sheetsNames = ((LCMSParserXLS) parserName).getSheetNames(fileDir);
+				for (String Name : sheetsNames) {
+					try {
+						if (status != TaskStatus.CANCELED) {
+							parser = new LCMSParserXLS(fileDir, Name);
+							parser.fillData();
+							this.open(parser);
+						}
+					} catch (Exception exception) {
+						 exception.printStackTrace();
+					}
+				}
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		} else if (fileDir.matches(".*csv")) {
+			try {
+				if (status != TaskStatus.CANCELED) {
+					parser = new LCMSParserCSV(fileDir);
+					parser.fillData();
+					this.open(parser);
+				}
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+	}
+
+	public void open(Parser parser) {
+		try {
+			if (status != TaskStatus.CANCELED) {
+				SimpleLCMSDataset dataset = (SimpleLCMSDataset) parser.getDataset();
+				desktop.AddNewFile(dataset);
+
+				//creates internal frame with the table
+				DataTableModel model = new DatasetLCMSDataModel(dataset);
+				DataTable table = new PushableTable(model);
+				table.formatNumbers(dataset.getType());
+				DataInternalFrame frame = new DataInternalFrame(dataset.getDatasetName(), table.getTable(), new Dimension(800, 800));
+
+				desktop.addInternalFrame(frame);
+				frame.setVisible(true);
+			}
+		} catch (Exception exception) {
+			// exception.printStackTrace();
+		}
+	}
 }
