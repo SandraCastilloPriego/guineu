70079947800ea1920293ed35223c3b3b57572904
nolssi <nolssi@1e2eb27e-0179-11de-9992-cbdf800730d7>
2009-09-19 18:29:12 +0000

    --force-log

--- trunk/src/guineu/modules/file/openLCMSDatasetFile/DatasetOpenDialog.java
+++ trunk/src/guineu/modules/file/openLCMSDatasetFile/DatasetOpenDialog.java
@@ -1,22 +1,10 @@
 /*
- * Copyright 2007-2008 VTT Biotechnology
- * This file is part of Guineu.
- *
- * Guineu is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * Guineu is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * Guineu; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
- * Fifth Floor, Boston, MA 02110-1301 USA
- */
+Copyright 2007-2008 VTT Biotechnology
 
-package guineu.modules.file.openExperimentsData;
+This file is part of GUINEU.
+
+ */
+package guineu.modules.file.openLCMSDatasetFile;
 
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
@@ -32,6 +20,7 @@ import javax.swing.JOptionPane;
 import com.sun.java.ExampleFileFilter;
 import guineu.desktop.impl.DesktopParameters;
 import guineu.main.GuineuCore;
+import guineu.util.dialogs.ExitCode;
 
 /**
  * File open dialog
@@ -39,31 +28,32 @@ import guineu.main.GuineuCore;
 public class DatasetOpenDialog extends JDialog implements ActionListener {
 
 	private Logger logger = Logger.getLogger(this.getClass().getName());
-
 	private JFileChooser fileChooser;
-	private File datasetFile;
+	private File[] datasetFile;
+	private ExitCode exit = ExitCode.UNKNOWN;
 
 	public DatasetOpenDialog(File lastpath) {
 
 		super(GuineuCore.getDesktop().getMainFrame(),
-				"Please select a experiment file to open...", true);
+				"Please select a dataset file to open...", true);
 
-		logger.finest("Displaying experiment open dialog");
+		logger.finest("Displaying dataset open dialog");
 
-		fileChooser = new JFileChooser();		             
-                if (lastpath != null) {
+		fileChooser = new JFileChooser();		
+		if (lastpath != null) {
 			fileChooser.setCurrentDirectory(lastpath);
 		}
-		fileChooser.setMultiSelectionEnabled(false);
+		fileChooser.setMultiSelectionEnabled(true);
 		fileChooser.addActionListener(this);
-		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);		
-              
-                ExampleFileFilter xls = new ExampleFileFilter();
-		xls.addExtension("xls");
-		xls.setDescription("Excel Files");
-		fileChooser.addChoosableFileFilter(xls);                
-		fileChooser.setFileFilter(xls);
-                
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+		ExampleFileFilter csv = new ExampleFileFilter();
+		csv.addExtension("csv");
+		csv.addExtension("xls");
+		csv.setDescription("Excel and Comma Separated Files");
+		fileChooser.addChoosableFileFilter(csv);
+		fileChooser.setFileFilter(csv);
+
 		add(fileChooser, BorderLayout.CENTER);
 		pack();
 		setLocationRelativeTo(GuineuCore.getDesktop().getMainFrame());
@@ -78,24 +68,31 @@ public class DatasetOpenDialog extends JDialog implements ActionListener {
 
 		// check if user clicked "Open"
 
-		if (command.equals("ApproveSelection")) {			
+		if (command.equals("ApproveSelection")) {
 			try {
-                                datasetFile = fileChooser.getSelectedFile();
-                                DesktopParameters deskParameters = (DesktopParameters) GuineuCore
-                                                    .getDesktop().getParameterSet();
-                                deskParameters.setLastOpenProjectPath(datasetFile.getPath()); 
+				datasetFile = fileChooser.getSelectedFiles();
+				DesktopParameters deskParameters = (DesktopParameters) GuineuCore.getDesktop().getParameterSet();
+				deskParameters.setLastOpenProjectPath(datasetFile[0].getPath());
+				exit = ExitCode.OK;
 			} catch (Throwable e) {
 				JOptionPane.showMessageDialog(this,
-						"Could not open experiment file", "Dataset opening error",
+						"Could not open dataset file", "Dataset opening error",
 						JOptionPane.ERROR_MESSAGE);
-				logger.fine("Could not open experiment file." + e.getMessage());
+				logger.fine("Could not open dataset file." + e.getMessage());
+				exit = ExitCode.CANCEL;
 			}
+		} else {
+			exit = ExitCode.CANCEL;
 		}
 		// discard this dialog
 		dispose();
 	}
 
-	public String getCurrentDirectory() {
-		return this.datasetFile.toString();
+	public File[] getCurrentDirectory() {
+		return this.datasetFile;
+	}
+
+	public ExitCode getExitCode() {
+		return exit;
 	}
 }
--- trunk/src/guineu/modules/file/openLCMSDatasetFile/OpenFile.java
+++ trunk/src/guineu/modules/file/openLCMSDatasetFile/OpenFile.java
@@ -1,21 +1,10 @@
 /*
- * Copyright 2007-2008 VTT Biotechnology
- * This file is part of Guineu.
- *
- * Guineu is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * Guineu is distributed in the hope that it will be useful, but WITHOUT ANY
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
- * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with
- * Guineu; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
- * Fifth Floor, Boston, MA 02110-1301 USA
+Copyright 2007-2008 VTT Biotechnology
+
+This file is part of GUINEU.
+
  */
-package guineu.modules.file.openExperimentsData;
+package guineu.modules.file.openLCMSDatasetFile;
 
 import guineu.data.ParameterSet;
 import guineu.desktop.Desktop;
@@ -34,99 +23,92 @@ import java.awt.event.KeyEvent;
 import java.io.File;
 import java.util.logging.Logger;
 
-/**
- *
- * @author scsandra
- */
 public class OpenFile implements GuineuModule, TaskListener, ActionListener {
-   private Logger logger = Logger.getLogger(this.getClass().getName());
- 
 
-    private Desktop desktop;
-    private String FilePath;
-    
-    public void initModule() {
+	private Logger logger = Logger.getLogger(this.getClass().getName());
+	private Desktop desktop;
+	private File[] FilePath;
 
-        this.desktop = GuineuCore.getDesktop(); 
-        desktop.addMenuSeparator(GuineuMenu.FILE);
-        desktop.addMenuItem(GuineuMenu.FILE, "Open Experiment File..",
-                "TODO write description", KeyEvent.VK_P, this, null);
+	public void initModule() {
 
-    }
+		this.desktop = GuineuCore.getDesktop();
+		desktop.addMenuItem(GuineuMenu.FILE, "Open LCMS Local File..",
+				"TODO write description", KeyEvent.VK_L, this, null);
 
-    
-    public void taskStarted(Task task) {
-        logger.info("Running Experiment File");
-    }
+	}
 
-    public void taskFinished(Task task) {
-        if (task.getStatus() == Task.TaskStatus.FINISHED) {
-            logger.info("Finished Experiment file on "
-                    + ((OpenFileTask) task).getTaskDescription());
-        }
+	public void taskStarted(Task task) {
+		logger.info("Running Open File");
+	}
 
-        if (task.getStatus() == Task.TaskStatus.ERROR) {
+	public void taskFinished(Task task) {
+		if (task.getStatus() == Task.TaskStatus.FINISHED) {
+			logger.info("Finished open file on " + ((OpenFileTask) task).getTaskDescription());
+		}
 
-            String msg = "Error while Experiment file on .. "
-                    + ((OpenFileTask) task).getErrorMessage();
-            logger.severe(msg);
-            desktop.displayErrorMessage(msg);
+		if (task.getStatus() == Task.TaskStatus.ERROR) {
 
-        }
-    }
+			String msg = "Error while open file on .. " + ((OpenFileTask) task).getErrorMessage();
+			logger.severe(msg);
+			desktop.displayErrorMessage(msg);
 
-    public void actionPerformed(ActionEvent e) {
-        ExitCode exitCode = setupParameters();
-        if (exitCode != ExitCode.OK)
-            return;
+		}
+	}
 
-        runModule(null);
-    }
+	public void actionPerformed(ActionEvent e) {
+		ExitCode exitCode = setupParameters();
+		if (exitCode != ExitCode.OK) {
+			return;
+		}
 
-    public ExitCode setupParameters() {
-        DesktopParameters deskParameters = (DesktopParameters) GuineuCore
-						.getDesktop().getParameterSet();
-        String lastPath = deskParameters.getLastOpenProjectPath();
-        if (lastPath == null)
-                lastPath = "";
-        File lastFilePath = new File(lastPath);
-        DatasetOpenDialog dialog = new DatasetOpenDialog(lastFilePath);
-        dialog.setVisible(true);
-        try{
-            this.FilePath = dialog.getCurrentDirectory();      
-        }catch(Exception e){}
-        return ExitCode.OK;
-    }
+		runModule(null);
+	}
 
-    public ParameterSet getParameterSet() {
-        return null;
-    }
+	public ExitCode setupParameters() {
+		DesktopParameters deskParameters = (DesktopParameters) GuineuCore.getDesktop().getParameterSet();
+		String lastPath = deskParameters.getLastOpenProjectPath();
+		if (lastPath == null) {
+			lastPath = "";
+		}
+		File lastFilePath = new File(lastPath);
+		DatasetOpenDialog dialog = new DatasetOpenDialog(lastFilePath);
+		dialog.setVisible(true);
+		try {
+			this.FilePath = dialog.getCurrentDirectory();
+		} catch (Exception e) {
+		}
+		return dialog.getExitCode();
+	}
 
-    public void setParameters(ParameterSet parameterValues) {
-        
-    }
-    
-    public String toString() {
-        return "Open Experiment File";
-    }
+	public ParameterSet getParameterSet() {
+		return null;
+	}
 
-    public TaskGroup runModule( TaskGroupListener taskGroupListener) {
-        
-        // prepare a new group of tasks
-        if(FilePath != null){
-            Task tasks[] = new OpenFileTask[1];       
-            tasks[0] = new OpenFileTask(FilePath, desktop);
+	public void setParameters(ParameterSet parameterValues) {
+	}
 
-            TaskGroup newGroup = new TaskGroup(tasks, this, taskGroupListener);
+	public String toString() {
+		return "Open File";
+	}
 
-            // start the group
-            newGroup.start();
+	public TaskGroup runModule(TaskGroupListener taskGroupListener) {
 
-            return newGroup;
-        }else return null;
+		// prepare a new group of tasks
+		if (FilePath != null) {
+			Task tasks[] = new OpenFileTask[FilePath.length];
+			for (int i = 0; i < FilePath.length; i++) {
 
-    }
-    
-  
+				tasks[i] = new OpenFileTask(FilePath[i].toString(), desktop);
+			}
+			TaskGroup newGroup = new TaskGroup(tasks, this, taskGroupListener);
 
+			// start the group
+			newGroup.start();
+
+			return newGroup;
+		} else {
+			return null;
+		}
+
+	}
 }
