971ab5130fc6eaa700a91df1295683c5e1b37dfe
nolssi <nolssi@1e2eb27e-0179-11de-9992-cbdf800730d7>
2010-04-15 10:09:48 +0000

    New module: Remove Non Polar Compounds

---
+++ src/guineu/modules/mylly/filter/NonPolarComponents/removeNonPolarFilter.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright 2007-2008 VTT Biotechnology
+ * This file is part of Guineu.
+ *
+ * Guineu is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Guineu is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Guineu; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package guineu.modules.mylly.filter.NonPolarComponents;
+
+import guineu.data.Dataset;
+import guineu.data.ParameterSet;
+import guineu.data.impl.SimpleGCGCDataset;
+import guineu.desktop.Desktop;
+import guineu.desktop.GuineuMenu;
+import guineu.main.GuineuCore;
+import guineu.main.GuineuModule;
+import guineu.taskcontrol.Task;
+import guineu.taskcontrol.TaskStatus;
+
+import guineu.taskcontrol.TaskListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author scsandra
+ */
+public class removeNonPolarFilter implements GuineuModule, TaskListener, ActionListener {
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+    private Desktop desktop;
+
+    public void initModule() {
+
+        this.desktop = GuineuCore.getDesktop();
+        desktop.addMenuItem(GuineuMenu.MYLLY, "Remove Non-Polar Compounds..",
+                "TODO write description", KeyEvent.VK_I, this, null, null);
+
+    }
+
+    public void taskStarted(Task task) {
+        logger.info("Running Remove Non-Polar Compounds");
+    }
+
+    public void taskFinished(Task task) {
+        if (task.getStatus() == TaskStatus.FINISHED) {
+            logger.info("Finished Remove Non-Polar Compounds on " + ((removeNonPolarFilterTask) task).getTaskDescription());
+        }
+
+        if (task.getStatus() == TaskStatus.ERROR) {
+
+            String msg = "Error while Remove Non-Polar Compounds on .. " + ((removeNonPolarFilterTask) task).getErrorMessage();
+            logger.severe(msg);
+            desktop.displayErrorMessage(msg);
+
+        }
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        runModule();
+    }
+
+    public ParameterSet getParameterSet() {
+        return null;
+    }
+
+    public void setParameters(ParameterSet parameterValues) {
+    }
+
+    public String toString() {
+        return "Remove Non-Polar Compounds";
+    }
+
+    public Task[] runModule() {
+
+        // prepare a new group of tasks
+        Dataset[] DataFiles = desktop.getSelectedDataFiles();
+        Task tasks[] = new removeNonPolarFilterTask[DataFiles.length];
+		for (int cont = 0; cont < DataFiles.length; cont++) {
+			tasks[cont] = new removeNonPolarFilterTask((SimpleGCGCDataset)DataFiles[cont]);
+		}
+		GuineuCore.getTaskController().addTasks(tasks);
+
+        return tasks;
+
+
+
+    }
+}
---
+++ src/guineu/modules/mylly/filter/NonPolarComponents/removeNonPolarFilterTask.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright 2007-2008 VTT Biotechnology
+ * This file is part of Guineu.
+ *
+ * Guineu is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Guineu is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Guineu; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package guineu.modules.mylly.filter.NonPolarComponents;
+
+import guineu.data.PeakListRow;
+import guineu.data.datamodels.DatasetGCGCDataModel;
+import guineu.data.impl.SimpleGCGCDataset;
+import guineu.main.GuineuCore;
+import guineu.taskcontrol.Task;
+import guineu.taskcontrol.TaskStatus;
+import guineu.util.Tables.DataTable;
+import guineu.util.Tables.DataTableModel;
+import guineu.util.Tables.impl.PushableTable;
+import guineu.util.internalframe.DataInternalFrame;
+import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author scsandra
+ */
+public class removeNonPolarFilterTask implements Task {
+
+    private TaskStatus status = TaskStatus.WAITING;
+    private String errorMessage;
+    private double progress = 0.0f;
+    private SimpleGCGCDataset dataset;
+
+    public removeNonPolarFilterTask(SimpleGCGCDataset dataset) {
+        this.dataset = dataset;
+    }
+
+    public String getTaskDescription() {
+        return "Remove Non-Polar Compounds... ";
+    }
+
+    public double getFinishedPercentage() {
+        return progress;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void cancel() {
+        status = TaskStatus.CANCELED;
+    }
+
+    public void run() {
+        try {
+            status = TaskStatus.PROCESSING;
+            SimpleGCGCDataset newAlignment = (SimpleGCGCDataset) dataset.clone();
+
+            List<PeakListRow> removedRows = new ArrayList<PeakListRow>();
+            for(PeakListRow row : newAlignment.getRows()){
+                String spectrum = (String) row.getVar("getSpectrumString");
+                if(spectrum.contains(" 73:")){
+                    String value = spectrum.substring(spectrum.indexOf(" 73:")+4);
+                    try{
+                        value = value.substring(0, value.indexOf(" , "));
+                    }catch(Exception e){
+                        value = value.substring(0, value.length() - 1);
+                    }
+
+                    double doubleValue = Double.parseDouble(value);
+                    if(doubleValue < 250){
+                        removedRows.add(row);
+                    }
+                }
+
+            }
+
+            for(PeakListRow row : removedRows){
+                newAlignment.removeRow(row);
+            }
+            GuineuCore.getDesktop().AddNewFile(newAlignment);
+
+            status = TaskStatus.FINISHED;
+        } catch (Exception e) {
+            status = TaskStatus.ERROR;
+            errorMessage = e.toString();
+            return;
+        }
+    }
+}
