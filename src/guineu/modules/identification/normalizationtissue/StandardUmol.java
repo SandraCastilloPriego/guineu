e54038d29990f5d13ba8419aead3ebdd234050a1
nolssi <nolssi@1e2eb27e-0179-11de-9992-cbdf800730d7>
2010-07-05 14:43:25 +0000

    Tissue Normalization generalization

--- src/guineu/modules/identification/normalizationtissue/StandardUmol.java
+++ src/guineu/modules/identification/normalizationtissue/StandardUmol.java
@@ -15,45 +15,60 @@
  * Guineu; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
  * Fifth Floor, Boston, MA 02110-1301 USA
  */
-
 package guineu.modules.identification.normalizationtissue;
 
-import java.util.Hashtable;
-import java.util.Vector;
+import guineu.data.PeakListRow;
+import guineu.util.Range;
 
 /**
  *
  * @author scsandra
  */
 public class StandardUmol {
-    double Cer = 0.0;
-    double GPCho = 0.0;
-    double GPEtn = 0.0;
-    double LysoGPCho = 0.0;
-    double TAG = 0.0;
-    String other = " ";
-    double otherValue = 0.0;
-    String other1 = " ";
-    double otherValue1 = 0.0;
-    Vector<Double> vCer;
-    Vector<Double> vGPCho;
-    Vector<Double> vGPEtn;
-    Vector<Double> vLysoGPCho;
-    Vector<Double> vTAG;
-    Vector<Double> vOtherValue;
-    Vector<Double> vOtherValue1;
-    public Hashtable<String,Double> Weights;    
-    
-    
-    public StandardUmol(){
-        this.vCer = new Vector<Double>();
-        this.vGPCho = new Vector<Double>();
-        this.vGPEtn = new Vector<Double>();
-        this.vLysoGPCho = new Vector<Double>();
-        this.vTAG = new Vector<Double>();
-        this.vOtherValue = new Vector<Double>();
-        this.vOtherValue1 = new Vector<Double>();
-        this.Weights = new Hashtable<String,Double>();
-        
+
+    private PeakListRow row;
+    private String name;
+    private double realAmount = 0.0;
+    private Range RTRange;
+
+    public StandardUmol(PeakListRow row) {
+        this.row = row;
+        this.name = (String) row.getVar("getName");
+        this.RTRange = new Range(0.0, 0.0);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRealAmount(double value) {
+        this.realAmount = value;
+    }
+
+    public void addValue(PeakListRow values) {
+        this.row = values;
+    }
+
+    public double getRealAmount() {
+        return realAmount;
+    }
+
+    public Double getIntensity(String experimentName) {
+        return (Double) row.getPeak(experimentName);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setRange(Range RTRange) {
+        try {
+            this.RTRange = RTRange;
+        } catch (Exception e) {
+        }
+    }
+
+    public Range getRange() {
+        return this.RTRange;
     }
 }
---
+++ src/guineu/modules/identification/normalizationtissue/UnknownsDataModel.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2007-2010 VTT Biotechnology
+ * This file is part of Guineu.
+ *
+ * Guineu is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Guineu is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Guineu; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package guineu.modules.identification.normalizationtissue;
+
+import guineu.util.Range;
+import java.util.Vector;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author scsandra
+ */
+public class UnknownsDataModel extends AbstractTableModel {
+
+    private String[] columns;
+    private Vector<String> name;
+    private Vector<String> value;//content all data
+    private int numColumns;
+    private int numRows;
+    Vector<StandardUmol> standards;
+
+    public UnknownsDataModel(Vector<StandardUmol> standards) {
+
+        this.standards = standards;
+
+        columns = new String[2];
+        columns[0] = "Standard Name";
+        columns[1] = "RT Range";
+        name = new Vector<String>();
+        value = new Vector<String>();
+        for (StandardUmol std : standards) {
+            name.addElement(std.getName());
+            value.addElement(std.getRange().toString());
+        }
+
+        numRows = name.size();
+        numColumns = 2;
+    }
+
+    public int getRowCount() {
+        return numRows;
+    }
+
+    public int getColumnCount() {
+        return numColumns;
+    }
+
+    @Override
+    public String getColumnName(int columnIndex) {
+        return columns[columnIndex];
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int row, int column) {
+        if (column == 0) {
+            name.setElementAt((String) aValue, row);
+        } else {
+            try {
+                value.setElementAt((String) aValue, row);
+            } catch (Exception e) {
+            }
+        }
+
+    }
+
+    @Override
+    public Class<?> getColumnClass(int c) {
+        if (getValueAt(0, c) != null) {
+            return getValueAt(0, c).getClass();
+        } else {
+            return String.class;
+        }
+    }
+
+    @Override
+    public boolean isCellEditable(int row, int column) {
+        return true;
+    }
+
+    public Object getValueAt(int row, int column) {
+        if (column == 0) {
+            return name.elementAt(row);
+        } else {
+            return value.elementAt(row);
+        }
+    }
+
+    public void fillStandards() {
+        for (int i = 0; i < this.name.size(); i++) {
+            this.standards.elementAt(i).setRange(new Range(this.value.elementAt(i)));
+        }
+    }
+
+    public void resetStandards() {
+        for (int i = 0; i < this.value.size(); i++) {
+            this.value.setElementAt(new Range(0, 0).toString(), i);
+        }
+        this.fireTableDataChanged();
+    }
+}
