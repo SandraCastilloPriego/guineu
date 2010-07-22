3b6cdc3a12844cfa91dbf3fcb587f273036efc29
nolssi <nolssi@1e2eb27e-0179-11de-9992-cbdf800730d7>
2010-07-22 14:40:14 +0000

    Fixing bug in the filter of the database

--- trunk/src/guineu/modules/database/openDataDB/CheckNode.java
+++ trunk/src/guineu/modules/database/openDataDB/CheckNode.java
@@ -22,54 +22,57 @@ import javax.swing.tree.DefaultMutableTreeNode;
 
 class CheckNode extends DefaultMutableTreeNode {
 
-    public final static int SINGLE_SELECTION = 0;
-    public final static int DIG_IN_SELECTION = 4;
-    protected int selectionMode;
-    protected boolean isSelected;
+        public final static int SINGLE_SELECTION = 0;
+        public final static int DIG_IN_SELECTION = 4;
+        protected int selectionMode;
+        protected boolean isSelected;
 
-    public CheckNode() {
-        this(null);
-    }
-
-    public CheckNode(Object userObject) {
-        this(userObject, true, false);
-    }
-
-    public CheckNode(Object userObject, boolean allowsChildren,
-            boolean isSelected) {
-        super(userObject, allowsChildren);
-        this.isSelected = isSelected;
-        setSelectionMode(DIG_IN_SELECTION);
-    }
-
-    public void setSelectionMode(int mode) {
-        selectionMode = mode;
-    }
-
-    public int getSelectionMode() {
-        return selectionMode;
-    }
-
-    public void setSelected(boolean isSelected) {
-        this.isSelected = isSelected;
-
-        if ((selectionMode == DIG_IN_SELECTION) && (children != null)) {
-            Enumeration e = children.elements();
-            while (e.hasMoreElements()) {
-                CheckNode node = (CheckNode) e.nextElement();
-                node.setSelected(isSelected);
-            }
+        public CheckNode() {
+                this(null);
         }
-    }
 
-    public boolean isSelected() {
-        return isSelected;
-    }
+        public CheckNode(Object userObject) {
+                this(userObject, true, false, DIG_IN_SELECTION);
+        }
 
-    // If you want to change "isSelected" by CellEditor,
+        public CheckNode(Object userObject, boolean allowsChildren,
+                boolean isSelected, int selectionMode) {
+                super(userObject, allowsChildren);
+                this.isSelected = isSelected;
+                setSelectionMode(selectionMode);
+        }
+
+        public void setSelectionMode(int mode) {
+                selectionMode = mode;
+        }
+
+        public int getSelectionMode() {
+                return selectionMode;
+        }
+
+        public void setSelectedOnlyMe(boolean isSelected) {
+                this.isSelected = isSelected;
+        }
+
+        public void setSelected(boolean isSelected) {
+                this.isSelected = isSelected;
+
+                if ((selectionMode == DIG_IN_SELECTION) && (children != null)) {
+                        Enumeration e = children.elements();
+                        while (e.hasMoreElements()) {
+                                CheckNode node = (CheckNode) e.nextElement();
+                                node.setSelected(isSelected);
+                        }
+                }
+        }
+
+        public boolean isSelected() {
+                return isSelected;
+        }
+        // If you want to change "isSelected" by CellEditor,
   /*
-    public void setUserObject(Object obj) { if (obj instanceof Boolean) {
-     * setSelected(((Boolean)obj).booleanValue()); } else {
-     * super.setUserObject(obj); } }
-     */
+        public void setUserObject(Object obj) { if (obj instanceof Boolean) {
+         * setSelected(((Boolean)obj).booleanValue()); } else {
+         * super.setUserObject(obj); } }
+         */
 }
