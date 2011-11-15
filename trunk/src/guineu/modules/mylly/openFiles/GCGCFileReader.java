6fff1c634b571cd0e4d24677a2534a84642b8d66
nolssi@gmail.com <nolssi@gmail.com@1e2eb27e-0179-11de-9992-cbdf800730d7>
2011-11-15 14:10:08 +0000

    GCGC files opening more general

--- trunk/src/guineu/modules/mylly/openFiles/GCGCFileReader.java
+++ trunk/src/guineu/modules/mylly/openFiles/GCGCFileReader.java
@@ -18,6 +18,7 @@
 package guineu.modules.mylly.openFiles;
 
 import com.csvreader.CsvReader;
+import guineu.data.GCGCColumnName;
 import guineu.modules.mylly.datastruct.ComparablePair;
 import guineu.modules.mylly.datastruct.GCGCDatum;
 import guineu.modules.mylly.datastruct.GCGCDatumWithConcentration;
@@ -40,9 +41,9 @@ public class GCGCFileReader {
         private boolean _findConcentration;
 
         public GCGCFileReader(String separator, boolean filterClassified) {
-		if(separator.equals("\\t")){
+                if (separator.equals("\\t")) {
                         _separator = '\t';
-		}else{
+                } else {
                         _separator = separator.toCharArray()[0];
                 }
                 _filterClassified = filterClassified;
@@ -53,22 +54,10 @@ public class GCGCFileReader {
         }
 
         public List<GCGCDatum> readGCGCDataFile(File file) throws IOException {
-                int rtPos;
-                int simPos;
-                int areaPos;
-                int casPos;
-                int namePos;
-                int riPos;
-                int ClassificationsPos;
-                int spectrumPos;
-                int quantMassPos;
-                int concentrationPos;
+
                 List<GCGCDatum> peaks;
 
                 FileReader fr = null;
-
-                rtPos = ClassificationsPos = riPos = spectrumPos = concentrationPos = -1;
-                areaPos = namePos = casPos = simPos = quantMassPos = -1;
 
                 try {
                         fr = new FileReader(file);
@@ -78,55 +67,20 @@ public class GCGCFileReader {
                         throw e2;
                 }
 
-        CsvReader reader = new CsvReader(fr,_separator);
+                CsvReader reader = new CsvReader(fr, _separator);
+                reader.readRecord();
+                String[] header = reader.getValues();
 
-                //First read the header-row
-                {
-
-                        reader.readHeaders();
-                        String headerRow[] = reader.getHeaders();
-
-                        for (int i = 0; i < headerRow.length; i++) {
-                                String str = headerRow[i];
-                                if (str.equals("R.T. (s)")) {
-                                        rtPos = i;
-                                } else if ("Sample Concentration".equals(str)) {
-                                        concentrationPos = i;
-                                } else if ("Area".equals(str)) {
-                                        areaPos = i;
-                                } else if ("CAS".equals(str)) {
-                                        casPos = i;
-                                } else if ("Name".equals(str)) {
-                                        namePos = i;
-                                } else if ("Similarity".equals(str)) {
-                                        simPos = i;
-                                } else if ("Retention Index".equals(str)) {
-                                        riPos = i;
-                                } else if (_filterClassified && "Classifications".equals(str)) {
-                                        ClassificationsPos = i;
-                                } else if ("Spectra".equals(str)) {
-                                        spectrumPos = i;
-                                } else if ("Quant Masses".equals(str)) {
-                                        quantMassPos = i;
-                                }
-                        }
-                }
                 // Then go through the file row at a time
                 {
                         String splitRow[];
                         peaks = new ArrayList<GCGCDatum>();
                         int cont = 1;
-            while(reader.readRecord()){
+                        while (reader.readRecord()) {
                                 splitRow = reader.getValues();
-                                boolean foundArea = false;
-                                boolean foundRT1 = false;
-                                boolean foundRT2 = false;
-                                boolean foundName = false;
                                 boolean foundRetentionIndex = false;
-                                boolean foundClassificationPos = false;
-                                boolean foundSpectrum = false;
-                                boolean foundQuantMass = false;
                                 boolean foundConc = false;
+                                boolean filter = false;
 
                                 double rt1;
                                 double rt2;
@@ -137,7 +91,7 @@ public class GCGCFileReader {
                                 boolean useConc = false;
                                 List<ComparablePair<Integer, Integer>> spectrum = null;
 
-				int similarity = 800;
+                                int similarity = 800;
                                 String CAS, name;
 
                                 rt1 = rt2 = area = retentionIndex = similarity = -1;
@@ -146,100 +100,96 @@ public class GCGCFileReader {
                                 CAS = "";
 
                                 for (int i = 0; i < splitRow.length; i++) {
-                                        String curStr = splitRow[i];
-					if (!(_filterClassified && i == ClassificationsPos) &&
-							"".equals(curStr)) {
-                                                continue;
-                                        }
+                                        String curStr = splitRow[i];                                        
 
-                                        if (i == rtPos) {
+                                        if (header[i].matches(GCGCColumnName.RT1.getRegularExpression())) {
                                                 String rts[] = curStr.split(",");
                                                 try {
                                                         rt1 = Double.parseDouble(rts[0]);
-                                                        foundRT1 = true;
                                                         rt2 = Double.parseDouble(rts[1]);
-                                                        foundRT2 = true;
+
                                                 } catch (NumberFormatException e) {
+                                                        e.printStackTrace();
+                                                        rt1 = 0;
+                                                        rt2 = 0;
                                                 }
-                                        } else if (i == riPos) {
+                                        } else if (header[i].matches(GCGCColumnName.RTI.getRegularExpression())) {
                                                 try {
                                                         retentionIndex = "".equals(curStr) ? 0 : Double.parseDouble(curStr);
                                                         foundRetentionIndex = true;
                                                 } catch (NumberFormatException e) {
+                                                        retentionIndex = 0;
                                                 }
-                                        } else if (i == areaPos) {
+                                        } else if (header[i].matches(".*Area.*")) {
                                                 try {
                                                         area = Double.parseDouble(curStr);
-                                                        foundArea = true;
+
                                                 } catch (NumberFormatException e) {
+                                                        area = 0;
                                                 }
-                                        } else if (i == concentrationPos) {
+                                        } else if (header[i].matches(".*Concentration .*")) {
                                                 if (!("".equals(curStr) || curStr == null)) {
                                                         try {
                                                                 conc = Double.parseDouble(curStr);
+                                                                foundConc = true;
                                                         } catch (NumberFormatException e) {
+                                                                conc = 0;
                                                         }
-                                                        foundConc = true;
+
                                                 }
-                                        } else if (i == casPos) {
+                                        } else if (header[i].matches(GCGCColumnName.CAS.getRegularExpression())) {
                                                 CAS = curStr;
-                                        } else if (i == namePos) {
+                                        } else if (header[i].matches(GCGCColumnName.NAME.getRegularExpression())) {
                                                 name = curStr;
-                                                foundName = true;
-                                        } else if (i == simPos) {
+                                        } else if (header[i].matches(".*Similarity.*")) {
                                                 try {
                                                         similarity = "".equals(curStr) ? 0 : Integer.parseInt(curStr);
                                                 } catch (NumberFormatException e) {
+                                                        similarity = 0;
                                                 }
-                                        } else if (_filterClassified && i == ClassificationsPos) {
-                                                foundClassificationPos = true;
+                                        } else if (_filterClassified && header[i].contains("Classification")) {
+
                                                 if (curStr != null && !"".equals(curStr)) {
                                                         //We have some classification for the peak so drop it
+                                                        filter = true;
                                                         break;
                                                 }
-                                        } else if (i == spectrumPos && _findSpectrum) {
+                                        } else if (header[i].matches(GCGCColumnName.SPECTRUM.getRegularExpression()) && _findSpectrum) {
                                                 try {
                                                         spectrum = parseSpectrum(curStr);
-                                                        foundSpectrum = true;
+
                                                 } catch (NumberFormatException e) {
                                                 }
-                                        } else if (i == quantMassPos) {
+                                        } else if (header[i].matches(GCGCColumnName.MASS.getRegularExpression())) {
                                                 try {
                                                         quantMass = Double.parseDouble(curStr);
-						} catch (NumberFormatException e) {
-						} finally {
-                                                        foundQuantMass = true;
-                                                }
+                                                } catch (NumberFormatException e) {
+                                                        quantMass = 0;
+                                                } 
                                         }
 
+                                }
+
+                                if (!filter) {
                                         if (similarity < 0) {
                                                 similarity = 800;
                                         }
-					if (foundArea && foundRT1 &&
-							foundRT2 && foundName && foundQuantMass &&
-							(!_findSpectrum || foundSpectrum) &&
-							(!_findConcentration || foundConc) &&
-							(!_filterClassified || foundClassificationPos)) {
-                                                if (!"Not Found".equals(name)) {
-                                                        GCGCDatum currentRow;
-                                                        if (foundConc) {
-                                                                currentRow = new GCGCDatumWithConcentration(cont++, rt1, rt2, retentionIndex,
-                                                                        quantMass, similarity, area, CAS, name, useConc, file.getName(),
-                                                                        spectrum, conc);
-                                                        } else if (foundRetentionIndex) {
-                                                                currentRow = new GCGCDatum(cont++, rt1, rt2, retentionIndex,
-                                                                        quantMass, similarity, area, conc, useConc,
-                                                                        CAS, name, file.getName(), spectrum);
-                                                        } else {
-                                                                currentRow = new GCGCDatum(cont++, rt1, rt2, quantMass,
-                                                                        area, conc, useConc, similarity, CAS, name, file.getName(), spectrum);
-                                                        }
-                                                        peaks.add(currentRow);
 
-                                                }
-                                                break;
+                                        GCGCDatum currentRow;
+                                        if (foundConc) {
+                                                currentRow = new GCGCDatumWithConcentration(cont++, rt1, rt2, retentionIndex,
+                                                        quantMass, similarity, area, CAS, name, useConc, file.getName(),
+                                                        spectrum, conc);
+                                        } else if (foundRetentionIndex) {
+                                                currentRow = new GCGCDatum(cont++, rt1, rt2, retentionIndex,
+                                                        quantMass, similarity, area, conc, useConc,
+                                                        CAS, name, file.getName(), spectrum);
+                                        } else {
+                                                currentRow = new GCGCDatum(cont++, rt1, rt2, quantMass,
+                                                        area, conc, useConc, similarity, CAS, name, file.getName(), spectrum);
                                         }
-                                }//End for-loop for (int i = 0; i < splitRow.length; i++)...
+                                        peaks.add(currentRow);
+                                }
                         }
                 }
                 return peaks;
@@ -248,13 +198,18 @@ public class GCGCFileReader {
         private List<ComparablePair<Integer, Integer>> parseSpectrum(String str) {
                 String peaks[] = str.split(" ");
                 ArrayList<ComparablePair<Integer, Integer>> peakList = new ArrayList<ComparablePair<Integer, Integer>>(peaks.length);
-                for (String st : peaks) {                       
+
+
+                for (String st : peaks) {
                         String tokens[] = st.split(":");
                         ComparablePair<Integer, Integer> p = new ComparablePair<Integer, Integer>(
                                 new Integer(tokens[0]),
                                 new Integer(tokens[1]));
                         peakList.add(p);
+
+
                 }
                 return peakList;
+
         }
 }
