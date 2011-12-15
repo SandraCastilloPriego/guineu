09335b4692bad4da9b750c20d03646db0220c6d4
nolssi@gmail.com <nolssi@gmail.com@1e2eb27e-0179-11de-9992-cbdf800730d7>
2011-12-15 11:47:08 +0000

    2way-Anova test using R

---
+++ src/guineu/modules/dataanalysis/twowayanova/TwoWayAnovaParameters.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2007-2011 VTT Biotechnology
+ * This file is part of Guineu.
+ *
+ * Guineu is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Guineu is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Guineu; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package guineu.modules.dataanalysis.twowayanova;
+
+import guineu.main.GuineuCore;
+import guineu.parameters.SimpleParameterSet;
+import guineu.parameters.UserParameter;
+import guineu.parameters.parametersType.ComboParameter;
+
+/**
+ *
+ * @author scsandra
+ */
+public class TwoWayAnovaParameters extends SimpleParameterSet {
+
+        private static String[] parameters = GuineuCore.getDesktop().getSelectedDataFiles()[0].getParametersName().toArray(new String[0]);
+        public static final ComboParameter<String> groups = new ComboParameter<String>(
+                "Select the phenotype",
+                "The groups to perform anova test will be taken from the choosen parameter", parameters);
+
+         public static final ComboParameter<String> timePoints = new ComboParameter<String>(
+                "Select the time points",
+                "The time points to perform anova test will be taken from the choosen parameter", parameters);
+
+        public TwoWayAnovaParameters(String[] parameters) {
+                super(new UserParameter[]{groups, timePoints});
+                groups.setChoices(parameters);
+        }
+}
---
+++ src/guineu/modules/dataanalysis/twowayanova/TwoWayAnovaTestTask.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2007-2011 VTT Biotechnology
+ * This file is part of Guineu.
+ *
+ * Guineu is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Guineu is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Guineu; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package guineu.modules.dataanalysis.twowayanova;
+
+import guineu.data.Dataset;
+import guineu.data.PeakListRow;
+import guineu.modules.R.RUtilities;
+import guineu.taskcontrol.AbstractTask;
+import guineu.taskcontrol.TaskStatus;
+import java.util.Enumeration;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.rosuda.JRI.Rengine;
+import org.rosuda.JRI.REXP;
+import org.rosuda.JRI.RList;
+import org.rosuda.JRI.RVector;
+
+/**
+ *
+ * @author scsandra
+ */
+public class TwoWayAnovaTestTask extends AbstractTask {
+
+        private Dataset dataset;
+        private String parameterGroup, parameterTime;
+        private int progress = 0;
+
+        public TwoWayAnovaTestTask(Dataset dataset, TwoWayAnovaParameters parameters) {
+                this.dataset = dataset;
+                parameterGroup = parameters.getParameter(TwoWayAnovaParameters.groups).getValue();
+                parameterTime = parameters.getParameter(TwoWayAnovaParameters.timePoints).getValue();
+        }
+
+        public String getTaskDescription() {
+                return "Performing Anova test... ";
+        }
+
+        public double getFinishedPercentage() {
+                return (float) progress / dataset.getNumberRows();
+        }
+
+        public void cancel() {
+                setStatus(TaskStatus.CANCELED);
+        }
+
+        public void run() {
+                setStatus(TaskStatus.PROCESSING);
+                try {
+                        final Rengine rEngine;
+                        try {
+                                rEngine = RUtilities.getREngine();
+                        } catch (Throwable t) {
+
+                                throw new IllegalStateException(
+                                        "2 way-Anova requires R but it couldn't be loaded (" + t.getMessage() + ')');
+                        }
+
+                        // Creates the data set
+                        rEngine.eval("dataset<- matrix(nrow =" + dataset.getNumberRows() + ",ncol=" + dataset.getNumberCols() + ")");
+
+                        // Creates the description of the samples data set
+                        rEngine.eval("pheno <- matrix(nrow =" + dataset.getNumberCols() + ",ncol=" + 3 + ")");
+
+
+                        Vector<String> columnNames = dataset.getAllColumnNames();
+                        String dataColNames = "c(";                 
+
+                        // assing the values to the matrix
+                        for (int row = 0; row < dataset.getNumberRows(); row++) {
+                                PeakListRow peakRow = dataset.getRow(row);
+                                int r = row + 1;
+                                for (int column = 0; column < dataset.getNumberCols(); column++) {
+                                        int c = column + 1;
+
+                                        String colName = columnNames.elementAt(column);
+                                        double value = 0.0;
+                                        try {
+                                                value = (Double) peakRow.getPeak(colName);
+                                        } catch (Exception e) {
+                                               System.out.println(peakRow.getPeak(colName));
+                                        }
+                                 
+                                        if (!Double.isInfinite(value) && !Double.isNaN(value)) {
+
+                                                rEngine.eval("dataset[" + r + "," + c + "] = " + value);
+                                        } else {
+
+                                                rEngine.eval("dataset[" + r + "," + c + "] = NA");
+                                        }                                        
+                                }
+                        }
+                        for (int column = 0; column < dataset.getNumberCols(); column++) {
+                                int c = column + 1;
+                                String colName = columnNames.elementAt(column);
+                                dataColNames += "\"" + colName + "\", ";
+                                rEngine.eval("pheno[" + c + ",1]<- \"" + colName + "\"");
+                                rEngine.eval("pheno[" + c + ",2]<- \"" + dataset.getParametersValue(colName, this.parameterGroup) + "\"");
+                                rEngine.eval("pheno[" + c + ",3]<- \"" + dataset.getParametersValue(colName, this.parameterTime) + "\"");
+
+                        }
+
+                        dataColNames = dataColNames.substring(0, dataColNames.length() - 2);
+                        dataColNames += ")";
+
+                        rEngine.eval("colnames(dataset)<- " + dataColNames);
+                       
+                        rEngine.eval("write.csv(dataset, \"Dataset.csv\")");
+
+                        rEngine.eval("rownames(pheno)<- " + dataColNames);
+                        rEngine.eval("colnames(pheno)<- c(\"SampleName\", \"Phenotype\" , \"Time\")");
+                        rEngine.eval("pheno <- data.frame(pheno)");
+
+                        rEngine.eval("write.csv(pheno, \"pheno.csv\")");
+
+                     
+                        for (int row = 1; row < dataset.getNumberRows() + 1; row++) {
+                                try {
+                                        rEngine.eval("one.2 <- data.frame(\"Profile\"=as.numeric(unlist(dataset[" + row + ",])),\"Phenotype\"=factor(pheno[,\"Phenotype\"]), \"Time\"=factor(pheno[,\"Time\"]))");
+                                        rEngine.eval("one.aov <- aov(Profile ~ Phenotype*Time, data=one.2)");
+                                        rEngine.eval("p<- summary(one.aov)[[1]][\"Phenotype:Time\", \"Pr(>F)\"] ");
+                                        rEngine.eval("fdr <- p.adjust(p, method=\"BH\")");
+                                        REXP x = rEngine.eval("p");
+                                        double p = x.asDouble();
+                                        this.dataset.getRow(row - 1).setVar("setPValue", p);                                        
+                                        REXP qexp = rEngine.eval("fdr");
+                                        double q = qexp.asDouble();
+                                        this.dataset.getRow(row - 1).setVar("setQValue", q);
+                                        
+                                        if (p < 0.05 && q < 0.05) {
+                                                this.dataset.getRow(row - 1).setSelectionMode(true);
+                                        }
+                                } catch (Exception ex) {
+                                }
+                        }
+
+
+                        setStatus(TaskStatus.FINISHED);
+                } catch (Exception ex) {
+                        Logger.getLogger(TwoWayAnovaTestTask.class.getName()).log(Level.SEVERE, null, ex);
+                        setStatus(TaskStatus.ERROR);
+                }
+        }
+}
