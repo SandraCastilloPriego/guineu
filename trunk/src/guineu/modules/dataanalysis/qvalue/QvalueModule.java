9ea16c25812b7118bf64d6594d47236807ba9b9b
nolssi@gmail.com <nolssi@gmail.com@1e2eb27e-0179-11de-9992-cbdf800730d7>
2011-12-19 11:35:48 +0000

    q-value module

---
+++ trunk/src/guineu/modules/dataanalysis/qvalue/QvalueModule.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2007-2011 VTT Biotechnology
+ * This file is part of Guineu.
+ *
+ * Guineu is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Guineu is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Guineu; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package guineu.modules.dataanalysis.qvalue;
+
+import guineu.main.GuineuCore;
+import guineu.modules.GuineuModuleCategory;
+import guineu.taskcontrol.Task;
+import guineu.data.Dataset;
+import guineu.modules.GuineuProcessingModule;
+import guineu.parameters.ParameterSet;
+
+/**
+ *
+ * @author scsandra
+ */
+public class QvalueModule implements GuineuProcessingModule {
+
+        public static final String MODULE_NAME = "Calculate Q-value";
+
+        public ParameterSet getParameterSet() {
+                return null;
+        }
+
+        public String toString() {
+                return MODULE_NAME;
+        }
+
+        public Task[] runModule(ParameterSet parameters) {
+                Dataset[] DataFiles = GuineuCore.getDesktop().getSelectedDataFiles();
+                // prepare a new group of tasks
+                Task tasks[] = new QvalueTask[1];
+                tasks[0] = new QvalueTask(DataFiles[0]);
+                GuineuCore.getTaskController().addTasks(tasks);
+                return tasks;
+        }
+
+        public GuineuModuleCategory getModuleCategory() {
+                return GuineuModuleCategory.DATAANALYSIS;
+        }
+
+        public String getIcon() {
+                return null;
+        }
+
+        public boolean setSeparator() {
+                return false;
+        }
+}
---
+++ trunk/src/guineu/modules/dataanalysis/qvalue/QvalueTask.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2007-2011 VTT Biotechnology
+ * This file is part of Guineu.
+ *
+ * Guineu is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Guineu is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Guineu; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package guineu.modules.dataanalysis.qvalue;
+
+import guineu.data.Dataset;
+import guineu.data.PeakListRow;
+import guineu.modules.R.RUtilities;
+import guineu.taskcontrol.AbstractTask;
+import guineu.taskcontrol.TaskStatus;
+import java.util.Vector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.rosuda.JRI.Rengine;
+import org.rosuda.JRI.REXP;
+
+/**
+ *
+ * @author scsandra
+ */
+public class QvalueTask extends AbstractTask {
+
+        private Dataset dataset;       
+        private int progress = 0;
+
+        public QvalueTask(Dataset dataset) {
+                this.dataset = dataset;                
+        }
+
+        public String getTaskDescription() {
+                return "Calculating q-value... ";
+        }
+
+        public double getFinishedPercentage() {
+                return (float) progress / dataset.getNumberRows();
+        }
+
+        public void cancel() {
+                setStatus(TaskStatus.CANCELED);
+        }
+
+        public void run() {
+                setStatus(TaskStatus.PROCESSING);
+                try {
+                        final Rengine rEngine;
+                        try {
+                                rEngine = RUtilities.getREngine();
+                        } catch (Throwable t) {
+
+                                throw new IllegalStateException(
+                                        "q-value test requires R but it couldn't be loaded (" + t.getMessage() + ')');
+                        }
+
+                        rEngine.eval("p <- vector(mode=\"numeric\",length="+dataset.getNumberRows()+")");
+
+                       
+                        // assing the values to the matrix
+                        for (int row = 0; row < dataset.getNumberRows(); row++) {
+                                int r = row + 1;
+                                rEngine.eval("p["+r+"] <- "+ dataset.getRow(row).getVar("getPValue"));
+                        }                       
+
+                        rEngine.eval("fdr <- p.adjust(p, method=\"BH\")");
+                        REXP qexp = rEngine.eval("fdr");
+                        double[] q = qexp.asDoubleArray();
+
+                        for (int row = 0; row < dataset.getNumberRows(); row++) {
+                                PeakListRow r = this.dataset.getRow(row);
+                                r.setVar("setQValue", q[row]);
+                                if ((Double)r.getVar("getPValue") < 0.05 && (Double)r.getVar("getQValue") < 0.05) {
+                                        this.dataset.getRow(row).setSelectionMode(true);
+                                }
+                        }
+
+                        setStatus(TaskStatus.FINISHED);
+                } catch (Exception ex) {
+                        Logger.getLogger(QvalueTask.class.getName()).log(Level.SEVERE, null, ex);
+                        setStatus(TaskStatus.ERROR);
+                }
+        }
+}
