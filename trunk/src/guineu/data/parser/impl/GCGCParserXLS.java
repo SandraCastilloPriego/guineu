2cf258c70c0e3f2b98f18802d407d2fdf1fc8cb2
nolssi@gmail.com <nolssi@gmail.com@1e2eb27e-0179-11de-9992-cbdf800730d7>
2012-01-18 09:35:14 +0000

    parser of XLS files for reading the parameters file

--- trunk/src/guineu/data/parser/impl/GCGCParserXLS.java
+++ trunk/src/guineu/data/parser/impl/GCGCParserXLS.java
@@ -36,203 +36,203 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 
 public class GCGCParserXLS extends ParserXLS implements Parser {
 
-    private String DatasetName;
-    private SimpleGCGCDataset dataset;
-    private Vector<String> head;
-    private HSSFWorkbook book;
-    private String sheetName;
-    private int numberRows,  rowsReaded;
+        private String DatasetName;
+        private SimpleGCGCDataset dataset;
+        private Vector<String> head;
+        private HSSFWorkbook book;
+        private String sheetName;
+        private int numberRows, rowsReaded;
 
-    public GCGCParserXLS(String DatasetName, String sheetName) {
-        this.numberRows = 0;
-        this.rowsReaded = 0;
-        this.DatasetName = DatasetName;
-        this.sheetName = sheetName;
-        this.dataset = new SimpleGCGCDataset(this.getDatasetName());
-		this.dataset.setType(DatasetType.GCGCTOF);
-        this.head = new Vector<String>();
-    }
+        public GCGCParserXLS(String DatasetName, String sheetName) {
+                this.numberRows = 0;
+                this.rowsReaded = 0;
+                this.DatasetName = DatasetName;
+                this.sheetName = sheetName;
+                this.dataset = new SimpleGCGCDataset(this.getDatasetName());
+                this.dataset.setType(DatasetType.GCGCTOF);
+                this.head = new Vector<String>();
+        }
 
-    public void fillData() {
-        try {
-            book = this.openExcel(DatasetName);
-            HSSFSheet sheet;
-            try {
-                sheet = book.getSheet(sheetName);
-            } catch (Exception exception) {
-                sheet = book.getSheetAt(0);
-            }
+        public void fillData() {
+                try {
+                        book = this.openExcel(DatasetName);
+                        HSSFSheet sheet;
+                        try {
+                                sheet = book.getSheet(sheetName);
+                        } catch (Exception exception) {
+                                sheet = book.getSheetAt(0);
+                        }
 
-            int initRow = this.getRowInit(sheet);          
-            
-            if (initRow > -1) {
-                numberRows = this.getNumberRows(initRow, sheet);
-                HSSFRow row = sheet.getRow(initRow);
+                        int initRow = this.getRowInit(sheet);
 
+                        if (initRow > -1) {
+                                numberRows = this.getNumberRows(initRow, sheet);
+                                HSSFRow row = sheet.getRow(initRow);
+
+                                for (int i = 0; i < row.getLastCellNum(); i++) {
+                                        HSSFCell cell = row.getCell(i);
+                                        this.head.addElement(cell.toString());
+                                }
+                                this.readMetabolites(initRow + 1, numberRows, sheet);
+
+                                this.setExperimentsName(head);
+                        } else {
+                                this.dataset = null;
+                        }
+                } catch (IOException ex) {
+                        Logger.getLogger(GCGCParserXLS.class.getName()).log(Level.SEVERE, null, ex);
+                }
+        }
+
+        /**
+         * Reads lipid information.
+         * @param initRow
+         * @param numberRows
+         * @param numberCols
+         * @param sheet
+         */
+        public void readMetabolites(int initRow, int numberRows, HSSFSheet sheet) {
+                for (int i = initRow; i < numberRows + initRow; i++) {
+                        HSSFRow row = sheet.getRow(i);
+                        this.readRow(row);
+                        this.rowsReaded++;
+                }
+        }
+
+        private Object getType(String data, ParameterType type) {
+                try {
+                        switch (type) {
+                                case BOOLEAN:
+                                        return new Boolean(data);
+                                case INTEGER:
+                                        return Integer.valueOf(data);
+                                case DOUBLE:
+                                        return Double.valueOf(data);
+                                case STRING:
+                                        return data;
+                        }
+                        return null;
+                } catch (Exception e) {
+                        return null;
+                }
+        }
+
+        /**
+         * Reads lipid information of one row.
+         * @param row
+         * @param numberCols
+         * @return
+         */
+        public void readRow(HSSFRow row) {
+                HSSFCell cell;
+                SimplePeakListRowGCGC metabolite = new SimplePeakListRowGCGC();
                 for (int i = 0; i < row.getLastCellNum(); i++) {
-                    HSSFCell cell = row.getCell((short) i);
-                    this.head.addElement(cell.toString());
+                        try {
+                                String title = head.elementAt(i);
+                                if (title == null) {
+                                        continue;
+                                }
+                                cell = row.getCell(i);
+                                boolean isfound = false;
+
+                                for (GCGCColumnName field : GCGCColumnName.values()) {
+                                        if (title.matches(field.getRegularExpression())) {
+                                                metabolite.setVar(field.getSetFunctionName(), this.getType(cell.toString(), field.getType()));
+                                                isfound = true;
+                                                break;
+                                        }
+                                }
+
+                                if (!isfound) {
+                                        try {
+                                                metabolite.setPeak(title, cell.getNumericCellValue());
+                                        } catch (Exception e) {
+                                                e.printStackTrace();
+                                                metabolite.setPeak(title, 0.0);
+                                        }
+                                }
+
+                                if (metabolite.getName() == null) {
+                                        metabolite.setName("unknown");
+                                }
+                        } catch (Exception exception) {
+                                //exception.printStackTrace();
+                        }
                 }
-                this.readMetabolites(initRow + 1, numberRows, sheet);
-
-                this.setExperimentsName(head);
-            } else {
-                this.dataset = null;
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(GCGCParserXLS.class.getName()).log(Level.SEVERE, null, ex);
+                this.dataset.addRow(metabolite);
         }
-    }
 
-    /**
-     * Reads lipid information.
-     * @param initRow
-     * @param numberRows
-     * @param numberCols
-     * @param sheet
-     */
-    public void readMetabolites(int initRow, int numberRows, HSSFSheet sheet) {
-        for (int i = initRow; i < numberRows + initRow; i++) {
-            HSSFRow row = sheet.getRow(i);
-            this.readRow(row);
-            this.rowsReaded++;
-        }
-    }
+        /**
+         *
+         * @param sheet
+         * @return number of row which it starts to read the excel file.
+         */
+        public int getRowInit(HSSFSheet sheet) {
 
-    private Object getType(String data, ParameterType type) {
-        try {
-            switch (type) {
-                case BOOLEAN:
-                    return new Boolean(data);
-                case INTEGER:
-                    return Integer.valueOf(data);
-                case DOUBLE:
-                    return Double.valueOf(data);
-                case STRING:
-                    return data;
-            }
-            return null;
-        } catch (Exception e) {
-            return null;
-        }
-    }
+                Iterator rowIt = sheet.rowIterator();
+                int num = -1;
 
-    /**
-     * Reads lipid information of one row.
-     * @param row
-     * @param numberCols
-     * @return
-     */
-    public void readRow(HSSFRow row) {
-        HSSFCell cell;
-        SimplePeakListRowGCGC metabolite = new SimplePeakListRowGCGC();
-        for (int i = 0; i < row.getLastCellNum(); i++) {
-            try {
-                String title = head.elementAt(i);
-                if (title == null) {
-                    continue;
-                }
-                cell = row.getCell((short) i);
-                boolean isfound = false;
+                while (rowIt.hasNext()) {
+                        HSSFRow row = (HSSFRow) rowIt.next();
+                        HSSFCell cell = row.getCell(0);
 
-                for (GCGCColumnName field : GCGCColumnName.values()) {
-                    if (title.matches(field.getRegularExpression())) {
-                        metabolite.setVar(field.getSetFunctionName(), this.getType(cell.toString(), field.getType()));
-                        isfound = true;
-                        break;
-                    }
+                        if (cell != null) {
+                                for (GCGCColumnName field : GCGCColumnName.values()) {
+                                        if (cell.toString().matches(field.getRegularExpression())) {
+                                                num = row.getRowNum();
+                                                break;
+                                        }
+                                }
+                        }
                 }
 
-                if (!isfound) {
-                    try {
-                        metabolite.setPeak(title, cell.getNumericCellValue());
-                    } catch (Exception e) {
-						e.printStackTrace();
-                        metabolite.setPeak(title, 0.0);
-                    }
+                return num;
+        }
+
+        public String getDatasetName() {
+                return "GCxGC - " + this.getDatasetName(DatasetName) + " - " + sheetName;
+        }
+
+        public String[] getSheetNames(String fileName) throws IOException {
+                HSSFWorkbook wb = this.openExcel(fileName);
+                String[] sheetsNames = new String[wb.getNumberOfSheets()];
+                for (int i = 0; i < wb.getNumberOfSheets(); i++) {
+                        sheetsNames[i] = wb.getSheetName(i);
                 }
+                return sheetsNames;
+        }
 
-                if (metabolite.getName() == null) {
-                    metabolite.setName("unknown");
+        private void setExperimentsName(Vector<String> header) {
+                try {
+
+                        String regExpression = "";
+                        for (GCGCColumnName value : GCGCColumnName.values()) {
+                                regExpression += value.getRegularExpression() + "|";
+                        }
+
+                        for (int i = 0; i < header.size(); i++) {
+                                if (!header.elementAt(i).matches(regExpression)) {
+                                        this.dataset.addColumnName(header.elementAt(i));
+                                }
+                        }
+
+                } catch (Exception exception) {
                 }
-            } catch (Exception exception) {
-                //exception.printStackTrace();
-            }
         }
-        this.dataset.addRow(metabolite);
-    }
 
-    /**
-     *
-     * @param sheet
-     * @return number of row which it starts to read the excel file.
-     */
-    public int getRowInit(HSSFSheet sheet) {
-
-        Iterator rowIt = sheet.rowIterator();
-        int num = -1;
-
-        while (rowIt.hasNext()) {
-            HSSFRow row = (HSSFRow) rowIt.next();
-            HSSFCell cell = row.getCell((short) 0);
-
-            if (cell != null) {
-                for (GCGCColumnName field : GCGCColumnName.values()) {
-                    if (cell.toString().matches(field.getRegularExpression())) {
-                        num = row.getRowNum();
-                        break;
-                    }
+        public float getProgress() {
+                try {
+                        if (this.numberRows != 0) {
+                                return (float) this.rowsReaded / this.numberRows;
+                        } else {
+                                return 0.0f;
+                        }
+                } catch (Exception exception) {
+                        return 0.0f;
                 }
-            }
         }
 
-        return num;
-    }
-
-    public String getDatasetName() {
-        return "GCxGC - " + this.getDatasetName(DatasetName) + " - " + sheetName;
-    }
-
-    public String[] getSheetNames(String fileName) throws IOException {
-        HSSFWorkbook wb = this.openExcel(fileName);
-        String[] sheetsNames = new String[wb.getNumberOfSheets()];
-        for (int i = 0; i < wb.getNumberOfSheets(); i++) {
-            sheetsNames[i] = wb.getSheetName(i);
+        public Dataset getDataset() {
+                return this.dataset;
         }
-        return sheetsNames;
-    }
-
-    private void setExperimentsName(Vector<String> header) {
-        try {
-
-            String regExpression = "";
-            for (GCGCColumnName value : GCGCColumnName.values()) {
-                regExpression += value.getRegularExpression() + "|";
-            }
-
-            for (int i = 0; i < header.size(); i++) {
-                if (!header.elementAt(i).matches(regExpression)) {
-                    this.dataset.addColumnName(header.elementAt(i));
-                }
-            }
-
-        } catch (Exception exception) {
-        }
-    }
-
-    public float getProgress() {
-        try {
-            if (this.numberRows != 0) {
-                return (float) this.rowsReaded / this.numberRows;
-            } else {
-                return 0.0f;
-            }
-        } catch (Exception exception) {
-            return 0.0f;
-        }
-    }
-
-    public Dataset getDataset() {
-        return this.dataset;
-    }
 }
