7ead827d53454deaf5d8ce8db879b632ce3f73ff
nolssi@gmail.com <nolssi@gmail.com@1e2eb27e-0179-11de-9992-cbdf800730d7>
2012-02-09 08:08:10 +0000

    Log transformation filter

---
+++ trunk/src/guineu/modules/filter/logtransformation/LogTransformationTask.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2007-2012 VTT Biotechnology
+ * This file is part of Guineu.
+ *
+ * Guineu is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * Guineu is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Guineu; if not, write to the Free Software Foundation, Inc., 51 Franklin St,
+ * Fifth Floor, Boston, MA 02110-1301 USA
+ */
+package guineu.modules.filter.logtransformation;
+
+import guineu.data.Dataset;
+import guineu.data.PeakListRow;
+import guineu.taskcontrol.AbstractTask;
+import guineu.taskcontrol.TaskStatus;
+
+/**
+ *
+ * @author scsandra
+ */
+public class LogTransformationTask extends AbstractTask {
+
+        private double progress = 0.0f;
+        private Dataset dataset;        
+
+        public LogTransformationTask(Dataset simpleDataset) {
+                this.dataset = simpleDataset;
+        }
+
+        public String getTaskDescription() {
+                return "Log transformation... ";
+        }
+
+        public double getFinishedPercentage() {
+                return progress;
+        }
+
+        public void cancel() {
+                setStatus(TaskStatus.CANCELED);
+        }
+
+        public void run() {
+                try {
+                        setStatus(TaskStatus.PROCESSING);
+                        for (PeakListRow row : dataset.getRows()) {
+                                for (String experimentName : dataset.getAllColumnNames()) {
+                                        try {
+                                                Double peak = (Double) row.getPeak(experimentName);                                               
+                                                row.setPeak(experimentName,Math.log(peak));
+                                                
+                                        } catch (Exception e) {
+                                        }
+                                }
+                        }
+                        setStatus(TaskStatus.FINISHED);
+                } catch (Exception e) {
+                        setStatus(TaskStatus.ERROR);
+                        errorMessage = e.toString();
+                        return;
+                }
+        }
+}
