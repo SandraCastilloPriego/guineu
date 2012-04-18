e76a693201dcedf31f69b66e1cd4ae3272cae429
nolssi@gmail.com <nolssi@gmail.com@1e2eb27e-0179-11de-9992-cbdf800730d7>
2012-04-18 12:28:23 +0000

    Saving gene expression data.

--- trunk/src/guineu/data/impl/peaklists/SimplePeakListRowExpression.java
+++ trunk/src/guineu/data/impl/peaklists/SimplePeakListRowExpression.java
@@ -22,7 +22,6 @@ import java.awt.Color;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.List;
